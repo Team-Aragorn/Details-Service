9ed881354908538c306d8f00faa0f85a21ac52b0
melvinioso <matt.mcgavern@gmail.com>
2020-04-30 00:49:23 -0500

    Created mongoose schema and connected db to server

---
+++ server/database/index.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost/games', { useUnifiedTopology: true, useNewUrlParser: true });
+
+const gameSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  details: String,
+  images: [{ name: String, url: String }],
+  videos: [{ name: String, url: String }]
+});
+
+const Game = mongoose.model('Game', gameSchema);
