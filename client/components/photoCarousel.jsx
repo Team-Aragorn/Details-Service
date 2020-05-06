79d71bf5ffcedc8b33af88e40238889b2fbaf1c1
melvinioso <matt.mcgavern@gmail.com>
2020-05-05 23:28:55 -0500

    Fixed PhotoCarousel.jsx

---
+++ client/components/photoCarousel.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable import/extensions */
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
+import PropTypes from 'prop-types';
+import Photo from './Photo.jsx';
+
+const PhotoCarousel = ({ photos }) => (
+  <div className="carousel-container">
+    {photos.map((photo, key) => <Photo photo={photo} key={key} />)}
+  </div>
+);
+
+PhotoCarousel.propTypes = {
+  photos: PropTypes.arrayOf.isRequired,
+};
+
+export default PhotoCarousel;
